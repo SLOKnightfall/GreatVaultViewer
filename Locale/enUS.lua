d5fe19e4a9e3bcfdac149723090fafbe05bab3d8
SLOKnightfall <SLOKnightfall@gmail.com>
2021-03-09 13:03:04 -0800

    -ToC Update

--- Locale/enUS.lua
+++ Locale/enUS.lua
@@ -4,3 +4,5 @@ local L = _G.LibStub("AceLocale-3.0"):NewLocale(addonName, "enUS", true, true)
 if not L then return end
 
 L["Show Minimap Icon"] = true
+GREATVAULTVIEWER_BINDING_TOGGLE = "Toggle Great Vault"
+GREATVAULTVIEWER = "Great Vault Viewer"
